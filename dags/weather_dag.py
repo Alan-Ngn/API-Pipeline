fd92c7923760e32e5d9b13b79bf746ec09b75daf
Alan Nguyen <alanngn21@gmail.com>
2024-05-08 23:16:28 -0700

    added default args

--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -4,3 +4,17 @@ from airflow.providers.http.operators.http import HttpOperator
 from datetime import datetime, timedelta
 from airflow.providers.sqlite.hooks.sqlite import SqliteHook
 import json
+
+default_args = {
+    'owner':'snowglobe',
+    'start_date': datetime(2023, 2, 14, 7, 0, 0),  # datetime(year, month, day, hour, minute, second)
+    'retries': 1,
+    'retries_delay': timedelta(minutes=15)
+}
+
+@dag(
+    dag_id='openweathermap',
+    default_args=default_args,
+    schedule_interval=timedelta(days=1),
+    catchup=False
+)
