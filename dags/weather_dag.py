30eb1a7a65f9a8b187e1d3c0790b43e49e1eb93a
Alan Nguyen <alanngn21@gmail.com>
2024-05-09 23:45:18 -0700

    making the tasks

--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -18,3 +18,37 @@ default_args = {
     schedule_interval=timedelta(days=1),
     catchup=False
 )
+def weather_etl():
+    ski_resorts=[
+                    {
+                        'lat': 46.92930718381659,
+                        'lon':-121.50084437354852,
+                        'mountain': 'Crystal Mountain'
+                    },
+                    {
+                        'lat': 47.408923006793,
+                        'lon':-121.4130210782378,
+                        'mountain': 'Summit at Snoqualmie'
+                    },
+                    {
+                        'lat': 47.74457508492699,
+                        'lon':-121.08910872510756,
+                        'mountain': 'Stevens Pass'
+                    },
+                    {
+                        'lat': 39.63392622173591,
+                        'lon':-105.87151011534075,
+                        'mountain': 'Araphaoe Basin'
+                    },
+                    {
+                        'lat': 39.478058458168334,
+                        'lon':-106.16144782958219,
+                        'mountain': 'Copper Mountain'
+                    },
+                ]
+
+    OPENWEATHERMAP_API_KEY = Variable.get("OPENWEATHERMAP_API_KEY")
+
+    @task
+    def extract(api_results, mountain):
+        return [mountain]+json.loads(api_results)['list']
