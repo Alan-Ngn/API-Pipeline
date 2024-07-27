6d8e73a874e95c0a08ebdc8d061c5aecc1e96851
Alan Nguyen <alanngn21@gmail.com>
2024-07-26 17:26:30 -0700

    changed to 3 hour intervals

--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -12,14 +12,14 @@ import csv
 import json
 default_args = {
     'owner':'snowglobe',
-    'start_date': datetime(2023, 2, 14, 7, 0, 0),  # datetime(year, month, day, hour, minute, second)
+    'start_date': datetime(2023, 2, 14, 3, 0, 0),  # datetime(year, month, day, hour, minute, second)
     'retries': 1,
     'retries_delay': timedelta(minutes=15)
 }
 @dag(
     dag_id='openweathermap',
     default_args=default_args,
-    schedule_interval=timedelta(days=1),
+    schedule_interval=timedelta(hours=3),
     catchup=False
 )
 def weather_etl():
